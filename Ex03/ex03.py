8bd71f3b006ed91b45276343fe79ae3912b268d2
Royal-Babouche <134000247+Sheppst@users.noreply.github.com>
2024-05-13 13:11:44 +0200

    Update ex03.py

--- Ex03/ex03.py
+++ Ex03/ex03.py
@@ -2,9 +2,8 @@ def RevAlph() :
     a = ""
     for i in range(97,123,1) :
         a += chr(i)
-    while a != "" :
-        b += a[-1]
-        del a[-1]
+    for i in range(len(a)):
+        b += a[-i]
     print (b)
 
 RevAlph()
