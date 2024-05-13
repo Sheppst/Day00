6507f576cdd613664d1615c75da0e818fa1997b9
Royal-Babouche <134000247+Sheppst@users.noreply.github.com>
2024-05-13 14:51:59 +0200

    Update ex05.py

--- Ex05/ex05.py
+++ Ex05/ex05.py
@@ -5,14 +5,14 @@ def RevOrderIntTab ():
     RevTab = []
     for i in range(15):
         a = 0
-        a = r.randint(0,100000000)
+        a = r.randint(0,100)
         tab.append(a)
     for i in range (len(tab)) :
         for j in range (0 , len(tab) - i - 1) : 
             if tab[j] > tab[j+1]:
                 tab[j], tab[j+1] = tab[j+1], tab[j]
     for i in range (len(tab)) :
-        RevTab.append(tab[i])
+        RevTab.insert(0,tab[i])
 
     print (RevTab)
 
