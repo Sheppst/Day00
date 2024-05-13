28e7826d873f2cb79800a431ee30223209b757aa
Royal-Babouche <134000247+Sheppst@users.noreply.github.com>
2024-05-13 14:43:56 +0200

    Create ex04.py

---
+++ Ex04/ex04.py
@@ -0,0 +1,14 @@
+import random as r
+
+def OrderIntTab ():
+    tab = []
+    for i in range(15):
+        a = 0
+        a = r.randint()
+        tab.append(a)
+    for i in range (len(tab)) :
+        for j in range (0 , len(tab) - i - 1) : 
+            if tab[j] > tab[j+1]:
+                tab[j], tab[j+1] = tab[j+1], tab[j]
+
+
